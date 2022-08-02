64537efdd358902c7f1bf313e5ff56ee7084ab32
Kumar <2174752@cognizant.com>
2022-08-02 20:05:03 +0530

    first commit

--- src/main/java/com/example/demo/AzuremyappApplication.java
+++ src/main/java/com/example/demo/AzuremyappApplication.java
@@ -2,9 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AzuremyappApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzuremyappApplication.class, args);
