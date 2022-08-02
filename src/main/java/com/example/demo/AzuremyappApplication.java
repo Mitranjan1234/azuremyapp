0b53c1461f50c2234d43d6bbf28a5af9f66be131
Kumar <2174752@cognizant.com>
2022-08-02 19:59:45 +0530

    first commit

---
+++ src/main/java/com/example/demo/AzuremyappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuremyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuremyappApplication.class, args);
+	}
+
+}
